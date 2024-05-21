5c8a369f34f84b17f8c2a2b92c602238a18afdcf
kaoren1 <144876114+kaoren1@users.noreply.github.com>
2024-05-21 23:06:18 +0300

    a

---
+++ ProductPractic3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ProductPractic3.PractisDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductPractic3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PractisEntities pr = new PractisEntities();
+
+        WorkReportTableAdapter workReportTableAdapter = new WorkReportTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EF_Click(object sender, RoutedEventArgs e)
+        {
+            EFWindow ef = new EFWindow();
+            ef.Show();
+            ef.EFTable.ItemsSource = pr.Info.ToList();
+        }
+
+        private void DataSet_Click(object sender, RoutedEventArgs e)
+        {
+            DataSetWindow dataSetWindow = new DataSetWindow();
+            dataSetWindow.Show();
+            dataSetWindow.DataSetTable.ItemsSource = workReportTableAdapter.GetDataBy();
+        }
+    }
+}
---
+++ ProductPractic3/PractisDataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductPractic3
+{
+
+
+    partial class PractisDataSet
+    {
+    }
+}
+
+namespace ProductPractic3.PractisDataSetTableAdapters {
+    
+    
+    public partial class PeopleTableAdapter {
+    }
+}
